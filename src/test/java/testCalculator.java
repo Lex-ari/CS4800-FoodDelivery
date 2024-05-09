0de59eea81a8685523f9d34e16a67ae14f7e2fde
Lex-ari <amarianocello@gmail.com>
2024-05-09 16:18:14 -0700

    Init Commit

---
+++ src/test/java/testCalculator.java
@@ -0,0 +1,21 @@
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.print.Doc;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class testCalculator {
+
+    @Test
+    public void testThing(){
+        int expected = 1;
+        int actual = 1;
+        assertEquals(expected, actual);
+    }
+}
